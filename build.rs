3d11ab0ddbf40fbecbab9820720b425c213a79f2
Oleksandr Anyshchenko <oleksandr.anyshchenko@xdev.re>
2019-12-10 16:19:27 +0200

    chore(cargo): bump version to 0.2.1 (#5)

--- build.rs
+++ build.rs
@@ -12,9 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#[cfg(test)]
 use protoc_rust::Customize;
 
 fn main() {
+    #[cfg(test)]
     protoc_rust::run(protoc_rust::Args {
         out_dir: "tests/proto",
         input: &["tests/proto/message.proto"],
